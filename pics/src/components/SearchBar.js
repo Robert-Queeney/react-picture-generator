61838fda9749a6d6df82f46b60e653dbe734cfbc
scoslo5512 <robert.queeney@gmail.com>
2019-03-22 06:15:18 -0700

    Got initial search bar working

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    // onInputChange(event){
+    //     console.log(event.target.value)
+    // }
+
+    state = { term: ''};
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        //referencing the function that got passed down 
+        this.props.onSubmit(this.state.term)
+    }
+
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input 
+                            type="text" 
+                            value={this.state.term }
+                            onChange={(e) => this.setState({term: e.target.value})}>
+                        </input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
